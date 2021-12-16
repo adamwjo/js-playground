6bcc0d7912c88227dedbb657d8d6203df0144fca
Adam Johnson <adamwjo@gmail.com>
2021-12-16 08:36:09 -0500

    today's workshop

---
+++ array_methods/script.js
@@ -0,0 +1,63 @@
+console.log("connected to js file")
+
+const menu = [
+    {
+        name: "chicken",
+        qty: 10,
+        price: 15,
+        vegitarian: false
+    },
+    {
+        name: "steak",
+        qty: 4,
+        price: 22,
+        vegitarian: false
+    },
+    {
+        name: "brussel sprouts",
+        qty: 14,
+        price: 7,
+        vegitarian: true
+    },
+    {
+        name: "salad",
+        qty: 55,
+        price: 11,
+        vegitarian: true
+    },
+    {
+        name: "tuna",
+        qty: 4,
+        price: 33,
+        vegitarian: false
+    },
+    {
+        name: "cake",
+        qty: 5,
+        price: 6,
+        vegitarian: true
+    },
+    {
+        name: "grilled carrots",
+        qty: 20,
+        price: 11,
+        vegitarian: true
+    },
+    {
+        name: "pie",
+        qty: 2,
+        price: 7,
+        vegitarian: true
+    },
+]
+
+
+// A function that takes in the menu array as a argument, and returns all the
+// vegitarian items
+
+// A function that takes in the menu array as a argument, and adds 5 to the qty
+// of every item
+
+// A function that returns every menu item below a given price
+
+// A function that returns the menu object whos name matches the string given to that function
