31fe1060e4a0be94a886133de2ea418ec331410e
Adam Johnson <adamwjo@gmail.com>
2022-02-17 11:58:37 -0500

    todays exercise

---
+++ js_algos/README.md
@@ -0,0 +1,39 @@
+# How To Approach Data Structures and Algorithms
+
+## The Interview
+
+- The Contact Phase
+
+  - What happens here?
+    - Talk to recruiter
+    - Friend refers you (employee partnerships)
+    - Cold application
+  - What is evaluated?
+    - Work experience (even non code related)
+    - Side projects
+    - Who are you as a person?
+
+- The Interview Phase
+  - What happens here?
+    - Phone screen
+    - "Onsite" Interview
+    - Take home assignment
+  - What is evaluated?
+    - Are you a good culture fit?
+    - CAN YOU CODE?
+
+### How your ability to code will be evaluated
+
+1.) Take-home Assignment
+2.) Pair Programming
+3.) Whiteboarding
+
+### How to get better at whiteboarding?
+
+- practice
+- practice
+- practice
+- practice
+- practice
+
+Successful whiteboarding interviewee's have practiced enough to know "the tricks" to several algorithms. It is key to memorize a few simple algorithms. No one figures out the "fibonacci sequence" on the spot, instead a couple different solutions are practiced and memorized.
---
+++ js_algos/script.js
@@ -0,0 +1,43 @@
+// --- Directions
+// Given a string, return the character that is most
+// commonly used in the string.
+// --- Examples
+// maxChar("abcccccccd") === "c"
+// maxChar("apple 1231111") === "1"
+
+
+// --- Directions
+// Create a queue data structure.  The queue
+// should be a class with methods 'add' and 'remove'.
+// Adding to the queue should store an element until
+// it is removed
+// --- Examples
+//     const q = new Queue();
+//     q.add(1);
+//     q.remove(); // returns 1;
+
+
+// --- Directions
+// 1) Complete the task in weave/queue.js
+// 2) Implement the 'weave' function.  Weave
+// receives two queues as arguments and combines the
+// contents of each into a new, third queue.
+// The third queue should contain the *alterating* content
+// of the two queues.  The function should handle
+// queues of different lengths without inserting
+// 'undefined' into the new one.
+// *Do not* access the array inside of any queue, only
+// use the 'add', 'remove', and 'peek' functions.
+// --- Example
+//    const queueOne = new Queue();
+//    queueOne.add(1);
+//    queueOne.add(2);
+//    const queueTwo = new Queue();
+//    queueTwo.add('Hi');
+//    queueTwo.add('There');
+//    const q = weave(queueOne, queueTwo);
+//    q.remove() // 1
+//    q.remove() // 'Hi'
+//    q.remove() // 2
+//    q.remove() // 'There'
+
